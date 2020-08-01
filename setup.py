019bdfd0c753414891abf61604b81c78b92d388a
dsearle90 <dsearle90@hotmail.com>
2020-08-01 19:40:13 +1000

    edit setup config version

--- setup.py
+++ setup.py
@@ -2,10 +2,9 @@ from distutils.core import setup
 setup(
   name = 'markovattribution',
   packages = ['markovattribution'],
-  version = '0.1',
+  version = '0.3',
   license='MIT', 
   description = 'Markov modelling for channel attribution',
-  long_description=open('README.rst', 'rt').read(),
   author = 'David Searle',
   author_email = 'connect@deepleaf.com.au',
   url = 'https://github.com/dsearle90/MarkovAttribution',
